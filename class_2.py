be1ec1f9a72a29dd506601e0b0aa2b896f6fcc45
Abdul Basit <bbasitkdfbgvb@gmail.com>
2024-04-21 22:20:44 +0500

    Python_Class_02

---
+++ class_2.py
@@ -0,0 +1,57 @@
+# String method
+
+name:str = "Abdul Basit"
+print(name.upper())
+print(name.lower())
+
+# F String method
+
+first_name = "Abdul"
+last_name  = "Basit"
+full_name  = f"{first_name} {last_name}"
+print(full_name)
+
+# example 1
+
+first_name = "Abdul"
+last_name  = "Basit"
+full_name  = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+# example 2
+
+first_name = "Abdul"
+last_name  = "Basit"
+full_name  = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+
+# example
+
+name1 : str = "Abdul Basit"
+fname : str = "Abdul Dayyan"
+roll_number : int = 143
+education : str = "Metric"
+age: int = 19 
+
+card : str = f"""
+PIAIC Student Card
+student_Name : {name1}
+father_Name : {fname}
+roll_Number : {roll_number}
+Age : {age}
+Education : {education}
+"""
+print(card)
+
+# TRY IT YOURSELF ❤❤����
+
+
+
+msg = "I am Abdul Basit  and I am 19 years old."
+print(msg)
+
+
+
+
